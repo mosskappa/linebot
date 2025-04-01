1a0877ce72303229dca967f084465eaf0c22b97c
mosskappa <moss80199.cs05@nycu.edu.tw>
2025-04-01 09:39:52 +0800

    Update response.js

--- api/response.js
+++ api/response.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { pendingRequests } = require('./line-callback');
 
 module.exports = (req, res) => {
@@ -9,16 +8,4 @@ module.exports = (req, res) => {
     pendingRequests.delete(requestId);
   }
   res.send('回應已處理');
-=======
-const { pendingRequests } = require('./line-callback');
-
-module.exports = (req, res) => {
-  const { requestId, statusCode, body } = req.body;
-  if (pendingRequests.has(requestId)) {
-    const { res: originalRes } = pendingRequests.get(requestId);
-    originalRes.status(statusCode).send(body);
-    pendingRequests.delete(requestId);
-  }
-  res.send('回應已處理');
->>>>>>> 1378f029c3af3dcd13bacc6c7d37a0c19ad5b6b6
 };
