5df0a285ea267c480fad72c3e13b017f117f2050
mosskappa <moss80199.cs05@nycu.edu.tw>
2025-04-01 09:39:37 +0800

    Update poll.js

--- api/poll.js
+++ api/poll.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { pendingRequests } = require('./line-callback');
 
 module.exports = (req, res) => {
@@ -8,15 +7,4 @@ module.exports = (req, res) => {
   } else {
     res.json({ empty: true });
   }
-=======
-const { pendingRequests } = require('./line-callback');
-
-module.exports = (req, res) => {
-  if (pendingRequests.size > 0) {
-    const [requestId, { body }] = pendingRequests.entries().next().value;
-    res.json({ requestId, method: 'POST', url: '/e/phuc4z55jis8y9xx/', body });
-  } else {
-    res.json({ empty: true });
-  }
->>>>>>> 1378f029c3af3dcd13bacc6c7d37a0c19ad5b6b6
 };
